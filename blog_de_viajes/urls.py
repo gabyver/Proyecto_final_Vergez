fa300a19369b5b97614e8779d1d43b9821fa3ce9
Gabriela Vergez <gabyver@hotmail.com>
2023-05-21 12:56:31 -0300

    Agregada app blog, modelo y su primera  migración.Actualizada Base de Datos

--- blog_de_viajes/urls.py
+++ blog_de_viajes/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog_de_viajes.views import saludar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludar),
 ]
