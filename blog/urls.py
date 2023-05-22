dd17b077673b6434f05a6eb0d8f377ea9325aa98
Gabriela Vergez <gabyver@hotmail.com>
2023-05-21 22:03:25 -0300

    Agregados arreglos a vistas, urls y templates para poder crear articulos nuevos y listarlos

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import listar_articulos
+from blog.views import listar_articulos, crear_articulo, buscar_articulo
 
 urlpatterns = [
-    path('articulos/', listar_articulos, name="lista_articulos"),
+    path("articulos/", listar_articulos, name="lista_articulos"),
+    path("crear/", crear_articulo, name='crear_articulo'),
+    path("buscar/", buscar_articulo, name='buscar_articulo'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,63 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from blog.models import Articulo
+from blog.forms import ArticuloForm
+from django.contrib.auth.decorators import login_required
+    
+
 
 # Create your views here.
 def listar_articulos(request):
-    contexto={
-        'articulos': Articulo.objects.all()
+    articulos= Articulo.objects.all()
+    contexto = {
+        'articulos': articulos
     }
     http_response= render(
         request=request,
         template_name='blog/lista_articulos.html',
-        context= {'articulos': Articulo},
+        context= contexto,
     )
     return http_response
+
+
+@login_required
+def crear_articulo(request):
+    if request.method == 'POST':
+        form = ArticuloForm(request.POST)
+        if form.is_valid():
+            data= form.cleaned_data
+            titulo= data["titulo"]
+            subtitulo= data["subtitulo"]
+            cuerpo= data["cuerpo"]
+            autor= data["autor"]
+            articulo= Articulo.objects.create(titulo=titulo, subtitulo= subtitulo, cuerpo= cuerpo, autor=autor)
+            url_exitosa=reverse('lista_articulos')
+            return redirect(url_exitosa)
+    else:
+        form = ArticuloForm()
+    http_response= render(
+        request= request, 
+        template_name= 'blog/crear_articulo.html', 
+        context= {'form': form},
+        )
+    return http_response
+
+   
+def buscar_articulo(request):
+    if request.method == 'POST':
+        data= request.POST
+        busqueda= data['busqueda']
+        articulos= Articulo.objects.filter(titulo__icontains=busqueda)
+        contexto= {
+            'articulos': articulos,
+        }
+        http_response= render(
+            request= request, 
+            template_name= 'blog/lista_articulo.html', 
+            context=contexto,
+        )
+        return http_response
+    else:
+        return redirect('lista_articulos')
+
+
