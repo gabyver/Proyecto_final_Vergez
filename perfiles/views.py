2adbe97f6ff3a77760aedda5580a2644d72f3013
Gabriela Vergez <gabyver@hotmail.com>
2023-05-25 12:59:44 -0300

    Agregado login, logout y mejoras a la navegación (botones barra superior)

--- perfiles/views.py
+++ perfiles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
 from perfiles.forms import UserRegisterForm 
 
 # Create your views here.
@@ -22,4 +26,28 @@ def registro(request):
         context={'form':formulario}
         )
     
-    
+
+def login_view(request):
+    next_url=request.GET.get('next')
+    if request.method=="POST":
+        formulario=AuthenticationForm(request, data= request.POST)
+        if formulario.is_valid():
+            data=formulario.cleaned_data
+            usuario= data.get('username')
+            password=data.get('password')
+            user=authenticate(username=usuario, password=password)
+            if user:
+                login(request=request, user=user)
+            return redirect(reverse('inicio'))
+    else:
+        formulario=AuthenticationForm()
+    return render(
+        request=request,
+        template_name='perfiles/login.html',
+        context={'form':formulario},
+    )
+
+
+class CustomLogoutView(LogoutView):
+    template_name= 'perfiles/logout.html'
+    next_page=reverse_lazy('inicio')
